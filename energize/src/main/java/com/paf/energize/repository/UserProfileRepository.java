161e2731dd44a23f817c3849f50d35c4268725f0
MalshanD <malshanshalinda115@gmail.com>
2025-05-07 15:20:23 +0530

    make user profile repo

---
+++ energize/src/main/java/com/paf/energize/repository/UserProfileRepository.java
@@ -0,0 +1,13 @@
+package com.paf.energize.repository;
+
+
+import com.example.pafbackend.models.UserProfile;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
+    List<UserProfile> findByUserId(String userId);
+}
