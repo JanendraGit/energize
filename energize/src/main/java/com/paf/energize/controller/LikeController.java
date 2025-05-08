ada6ac06cb78da686b64695b2c7345bb12ce8d0a
JanendraGit <janendra.silva2001@gmail.com>
2025-05-08 10:19:16 +0530

    update controller use get

--- energize/src/main/java/com/paf/energize/controller/LikeController.java
+++ energize/src/main/java/com/paf/energize/controller/LikeController.java
@@ -1,7 +1,12 @@
 package com.paf.energize.controller;
+import com.paf.energize.model.Like;
 import com.paf.energize.repository.LikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/likes")
@@ -12,5 +17,11 @@ public class LikeController {
     @Autowired
     public LikeController(LikeRepository likeRepository) {
         this.likeRepository = likeRepository;
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<List<Like>> getLikesByPostId(@PathVariable String postId) {
+        List<Like> likes = likeRepository.findByPostId(postId);
+        return new ResponseEntity<>(likes, HttpStatus.OK);
     }
 }
