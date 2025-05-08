996541ba0c3b0d65192de385cbddb3e2bcdf1926
JanendraGit <janendra.silva2001@gmail.com>
2025-05-08 10:17:26 +0530

    like repo build

--- energize/src/main/java/com/paf/energize/controller/LikeController.java
+++ energize/src/main/java/com/paf/energize/controller/LikeController.java
@@ -1,14 +1,16 @@
 package com.paf.energize.controller;
+import com.paf.energize.repository.LikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/likes")
 public class LikeController {
 
     private final LikeRepository likeRepository;
+
+    @Autowired
+    public LikeController(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
 }
