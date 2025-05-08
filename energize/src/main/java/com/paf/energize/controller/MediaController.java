86cc42fe56a256c90e947d4cef1f053bf5b4d232
JanendraGit <janendra.silva2001@gmail.com>
2025-05-08 10:38:53 +0530

    create media controller update get

--- energize/src/main/java/com/paf/energize/controller/MediaController.java
+++ energize/src/main/java/com/paf/energize/controller/MediaController.java
@@ -1,9 +1,16 @@
 package com.paf.energize.controller;
 
+import com.paf.energize.model.Media;
 import com.paf.energize.repository.MediaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/media")
@@ -13,5 +20,11 @@ public class MediaController {
 
     public MediaController(MediaRepository mediaRepository) {
         this.mediaRepository = mediaRepository;
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<List<Media>> getMediaByPostId(@PathVariable String postId) {
+        List<Media> mediaList = mediaRepository.findByPostId(postId);
+        return new ResponseEntity<>(mediaList, HttpStatus.OK);
     }
 }
