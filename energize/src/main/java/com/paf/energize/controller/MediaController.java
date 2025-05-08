a6c13d35336bfbb37d373ec9eb95851bd656e800
JanendraGit <janendra.silva2001@gmail.com>
2025-05-08 10:37:47 +0530

    create media controller update

--- energize/src/main/java/com/paf/energize/controller/MediaController.java
+++ energize/src/main/java/com/paf/energize/controller/MediaController.java
@@ -1,4 +1,17 @@
 package com.paf.energize.controller;
 
+import com.paf.energize.repository.MediaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/media")
 public class MediaController {
+
+    private final MediaRepository mediaRepository;
+
+    public MediaController(MediaRepository mediaRepository) {
+        this.mediaRepository = mediaRepository;
+    }
 }
