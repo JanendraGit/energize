15b0b78594ec3cc86fc6a9a29e02a217d5fcaf3d
JanendraGit <janendra.silva2001@gmail.com>
2025-05-12 10:19:38 +0530

    update controller2

--- energize/src/main/java/com/paf/energize/controller/CommentController.java
+++ energize/src/main/java/com/paf/energize/controller/CommentController.java
@@ -21,8 +21,7 @@ public class CommentController {
 
    @PostMapping
    public Comment createComment(@RequestBody Comment comment) {
-//
-//        return commentRepository.save(comment);
+       return commentRepository.save(comment);
    }
 //
 //    @GetMapping
