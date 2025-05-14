d7859ff3ca322fb4cf1770142fa4d49328cfb5f4
MalshanD <malshanshalinda115@gmail.com>
2025-05-14 23:37:41 +0530

    fix errors

--- energize/src/main/java/com/paf/energize/config/OAuth2LoginSuccessHandler.java
+++ energize/src/main/java/com/paf/energize/config/OAuth2LoginSuccessHandler.java
@@ -42,7 +42,7 @@ public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSu
             );
             tokenDTO = tokenGenerator.createToken(userAuthentication);
 
-            // Set tokens and user info in cookies
+
             addCookie(response, "access_token", tokenDTO.getAccessToken(), 86400);      // 1 day
             addCookie(response, "refresh_token", tokenDTO.getRefreshToken(), 2592000); // 30 days
             addCookie(response, "user_id", tokenDTO.getUserId(), 2592000);             // 30 days
